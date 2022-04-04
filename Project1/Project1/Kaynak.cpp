93bf6058edf1c81c1f2b7fc9b8b8f412e7c6bcff
AyseAyhan <Ayse.Ayhan6777@gmail.com>
2022-04-04 17:45:57 +0300

    update kaynak.cpp
    I deleted all spaces.

--- Project1/Project1/Kaynak.cpp
+++ Project1/Project1/Kaynak.cpp
@@ -7,14 +7,10 @@ int main()
 	int* Z;
 	Z = &X; //Z, X'in adresine eþit
 	*Z = X; //Z iþaretçisi X'in deðerine eþit // Bu sayede X deðiþirse Z de deðiþir, Y etkilenmez.
-
 	cout << "1.STEP--> Variable X: " << X << "\tVariable Y: " << Y << "\tVariable Z: " << *Z << endl; //Ýlk deðerler
 	cout << endl;
 	X = 50; //Sadece X'in deðerini deðiþtirerek oluþacak deðiþime bakalým:
-
 	cout << "2.STEP--> Variable X: " << X << "\tVariable Y: " << Y << "\tVariable Z: " << *Z << endl; //*Z, X ile ayný deðiþime sahip olur, Y ise etkilenmez.
 	cout << endl;
-
-
 	system("pause");
 }
